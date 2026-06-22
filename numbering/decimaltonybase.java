05ade9775de03e338434f7082c7a1638f0a55621
SBTechLab <sbbhlani11@gmail.com>
2026-06-22 17:10:12 +0530

    base decimal problems

--- numbering/decimaltonybase.java
+++ numbering/decimaltonybase.java
@@ -1,22 +1,33 @@
-import java.util.*;
-public class decimaltonybase{
+import java.util.Scanner;
 
-    public static String decimalToAnyBase(int n, int b){
-        String ans = "";
-        while(n > 0){
-            int rem = n % b;
-            ans = rem + ans;
-            n = n / b;
-        }
-        return ans;
-    }
+public class decimaltonybase {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Scanner scn = new Scanner(System.in);
+
         int n = scn.nextInt();
         int b = scn.nextInt();
 
-        String result = decimalToAnyBase(n, b);
-        System.out.println(result);
+        int dn = getValueInBase(n, b);
+
+        System.out.println(dn);
+    }
+
+    public static int getValueInBase(int n, int b) {
+
+        int rv = 0;
+        int p = 1;
+
+        while (n > 0) {
+
+            int dig = n % b;
+            n = n / b;
+
+            rv = rv + dig * p;
+            p = p * 10;
+        }
+
+        return rv;
     }
 }
