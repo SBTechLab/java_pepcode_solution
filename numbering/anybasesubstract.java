9d7fe8076d93b846b0f482214ebe2b697c57e73d
SBTechLab <sbbhlani11@gmail.com>
2026-06-22 17:32:58 +0530

    base decimal problems

--- numbering/anybasesubstract.java
+++ numbering/anybasesubstract.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class anybasesub {
+public class anybasesubstract {
 
     public static void main (String[] args){
         Scanner scn  = new Scanner(System.in);
